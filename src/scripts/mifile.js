2fb9bebb8ef63c15028dff8c5b392882300b5285
anjoalo1@gmail.com <anjojalo1@gmail.com>
2023-08-25 13:44:00 -0500

    prims-desdeastro

---
+++ src/scripts/mifile.js
@@ -0,0 +1,15 @@
+import Prism from 'prismjs';
+
+// Import PrismJS extensions
+import 'prism-themes/themes/prism-shades-of-purple.css';
+import 'prismjs/components/prism-scss';
+
+
+// Import Prism JS
+import 'prismjs/plugins/line-numbers/prism-line-numbers';
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
+import 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace.js';
+
+// Highlight all matching syntax
+
+Prism.highlightAll();
